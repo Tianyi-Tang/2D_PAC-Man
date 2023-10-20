80af242e5dddee46010c254cf61d4ca486499da8
LI-YU WU <li-yu_wu@sfu.ca>
2023-10-20 12:14:57 -0700

    create maven project

---
+++ pawsandpumpkins/src/main/java/cmpt276/group4/App.java
@@ -0,0 +1,13 @@
+package cmpt276.group4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
