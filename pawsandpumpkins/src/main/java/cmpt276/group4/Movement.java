d377cdabe0c610d370c95c0c2e1a01e6a22a6572
Andy Tang <tta89@sfu.ca>
2023-10-21 09:31:38 -0700

    sumbit .gitignore, interface Movement and Position

---
+++ pawsandpumpkins/src/main/java/cmpt276/group4/Movement.java
@@ -0,0 +1,8 @@
+package cmpt276.group4;
+
+public interface Movement {
+    boolean moveTo(Position position);
+    boolean moveable(Position position);
+}
+
+
---
+++ pawsandpumpkins/src/main/java/cmpt276/group4/Position.java
@@ -0,0 +1,19 @@
+package cmpt276.group4;
+
+public class Position{
+    private int x_axis;
+    private int y_axis;
+
+    Position(int x_axis,int y_axis){
+        this.x_axis = x_axis;
+        this.y_axis = y_axis;
+    }
+
+    public int getX_axis(){
+        return x_axis;
+    }
+
+    public int getY_axis(){
+        return y_axis;
+    }
+}
