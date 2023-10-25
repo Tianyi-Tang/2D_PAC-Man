f05e507ce905256aec0071319b409900d4a72fa1
Nicholas Smart <nrs5@sfu.ca>
2023-10-25 09:21:38 -0700

    RoomInitialization main methods/functions added

--- pawsandpumpkins/src/main/java/cmpt276/group4/Room/RoomInitialization.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/Room/RoomInitialization.java
@@ -1,5 +1,23 @@
 package cmpt276.group4.Room;
 
+import cmpt276.group4.GameManager;
+
 public class RoomInitialization {
+    private int roomLength;
+    //private GameManager typeOfRooom;
     
+    // Getter for roomLength
+    public int getRoomLength() {
+        return roomLength;
+    }
+
+    // Setter for room Length
+    public void setRoomLength(int x) {
+        this.roomLength = x;
+    }
+
+    // Dont know if this is correct? should i be entering type of room?
+    public void createRoom() {
+        System.out.println("Create new room");
+    }
 }
