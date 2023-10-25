5df0f0683b0f969950d4d4a4555804d13da61623
Nicholas Smart <nrs5@sfu.ca>
2023-10-25 09:19:34 -0700

    Added main functions for door

--- pawsandpumpkins/src/main/java/cmpt276/group4/Room/Door.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/Room/Door.java
@@ -1,5 +1,26 @@
 package cmpt276.group4.Room;
 
 public class Door {
-    
+    // open variable
+    private boolean open;
+
+    // getter for open
+    public boolean getOpen() {
+        return open;
+    }
+
+    // setter for open
+    public void setOpen(boolean i) {
+        this.open = i;
+    }
+
+    // this function opens door by setting open to true
+    private void openDoor() {
+        this.open = true;
+    }
+
+    // if player available is true call private openDoor()
+    public void meetCondition() {
+
+    }
 }
