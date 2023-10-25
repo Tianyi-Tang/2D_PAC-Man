ab698b9c11c80ca3cbe0ef73146c5f3531c97547
Nicholas Smart <nrs5@sfu.ca>
2023-10-25 09:20:20 -0700

    RoomFactory main functions

--- pawsandpumpkins/src/main/java/cmpt276/group4/Room/RoomFactory.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/Room/RoomFactory.java
@@ -1,5 +1,22 @@
 package cmpt276.group4.Room;
 
+import cmpt276.group4.Room.RoomInitialization;
+
 public class RoomFactory {
-    
+
+    public Room createRoom(int roomLength) {
+        Room room = createRoom(roomLength);
+        return room;
+    }
+
+    public Obstacle createObstacle() {
+        return new Obstacle();
+    }
+
+    public Door createDoor() {
+        return new Door();
+    }
+
+
+
 }
