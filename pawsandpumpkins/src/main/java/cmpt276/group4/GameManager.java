7c0704341f95b607e1ac7dbfdddae33c6fe20b37
Nicholas Smart <nrs5@sfu.ca>
2023-10-24 20:42:57 -0700

    added GameManager file and typeOfRoom variable

---
+++ pawsandpumpkins/src/main/java/cmpt276/group4/GameManager.java
@@ -0,0 +1,40 @@
+package cmpt276.group4;
+
+public class GameManager {
+    // typeOfRoom
+    private int typeOfRoom;
+
+    // getter
+    public int getTypeOfRoom() {
+        return typeOfRoom;
+    }
+
+    // setter
+    public void setTypeOfRoom(int i) {
+        typeOfRoom = i;
+    }
+
+    //instance
+
+    //enemyNum
+
+    //gameRoom
+
+    //totalRewardNum
+
+    //initialgame
+
+    //postGame
+
+    //openDoor
+
+    //endGame
+
+    //EndGame
+
+    //getAllReward
+
+    //catchyByEnemy
+
+    //getInstance
+}
