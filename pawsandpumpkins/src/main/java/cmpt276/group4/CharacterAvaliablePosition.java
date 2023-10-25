5b5e0cb42853bcfd490cb8c6a8e3d08556554c4e
Nicholas Smart <nrs5@sfu.ca>
2023-10-25 11:44:25 -0700

    Added CharacterAvaliablePosition interface and implemented it

---
+++ pawsandpumpkins/src/main/java/cmpt276/group4/CharacterAvaliablePosition.java
@@ -0,0 +1,8 @@
+package cmpt276.group4;
+
+public interface CharacterAvaliablePosition {
+    boolean isPlayerAvailable();
+    boolean isPlayerEnemyAvailable();
+    boolean playerAvaliable = true;
+    boolean enemyAvaliable = true;
+}
