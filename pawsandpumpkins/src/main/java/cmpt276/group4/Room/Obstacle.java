84cfd8b205d28824fc862918da3609709fc4bef6
Nicholas Smart <nrs5@sfu.ca>
2023-10-25 13:25:30 -0700

    Added implements

--- pawsandpumpkins/src/main/java/cmpt276/group4/Room/Obstacle.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/Room/Obstacle.java
@@ -1,5 +1,38 @@
 package cmpt276.group4.Room;
 
-public class Obstacle {
+import cmpt276.group4.CharacterAvaliablePosition;
+import cmpt276.group4.Position;
+
+public class Obstacle implements CharacterAvaliablePosition {
+
+    // Should these be private???
+    boolean playerAccess;
+    boolean enemyTraversability;
+
+    //Should this have [] for array
+    Position position;
+
+    public Obstacle() {
+        this.playerAccess = true;
+        this.enemyTraversability = true;
+        this.position = new Position(5, 5);
+    }
     
+    @Override
+    public boolean getPlayerAvaliable() {
+        throw new UnsupportedOperationException("is player avaliable");
+    }
+
+    @Override
+    public boolean getEnemyAvaliable() {
+        throw new UnsupportedOperationException("is enemy avaliable");
+    }
+
+
+
+
+
+
+
 }
+
