fb246661cd0411d9aac7a69b18afeb41ff570716
Nicholas Smart <nrs5@sfu.ca>
2023-10-25 09:56:45 -0700

    added room class with variables

--- pawsandpumpkins/src/main/java/cmpt276/group4/Room/Room.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/Room/Room.java
@@ -1,5 +1,8 @@
 package cmpt276.group4.Room;
 
+import cmpt276.group4.Position;
 public class Room {
-    
+    private Position doorPosition;
+    private Obstacle[] obstacle;
+    private Door[] door;
 }
