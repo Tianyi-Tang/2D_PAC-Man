183f2978271c2aea7759aca37ae16a350a5640aa
Andy Tang <tta89@sfu.ca>
2023-11-05 13:38:09 -0800

    make the tile have image and show on the screen

--- pawsandpumpkins/src/main/java/cmpt276/group4/Room/Tile.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/Room/Tile.java
@@ -2,20 +2,33 @@ package cmpt276.group4.Room;
 
 import cmpt276.group4.CharacterAvaliablePosition;
 import cmpt276.group4.Position;
-import cmpt276.group4.RecordUsedPlace;
 import cmpt276.group4.WindowAndInput.GamePanel;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 
 public class Tile implements CharacterAvaliablePosition {
     private boolean playerAvaliable = true;
     private boolean enemyAvialiable = true;
     private boolean takenPlace = false;
     private Position location;
+    BufferedImage tileImage;
 
     public Tile(Position position){
         location = position;
+        initalTileImage();
+    }
+
+    private void initalTileImage(){
+        try {
+            tileImage = ImageIO.read(new File(System.getProperty("user.dir") + "/res/Tiles/tile1.png"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
     }
 
     @Override
@@ -40,8 +53,9 @@ public class Tile implements CharacterAvaliablePosition {
     
     @Override
     public void draw(Graphics2D g2){
-        g2.setColor(Color.white);
-        g2.fillRect(location.getX_axis(), location.getY_axis(), GamePanel.tileSize , GamePanel.tileSize);
+        if(tileImage != null)
+            g2.drawImage(tileImage, location.getX_axis(), location.getY_axis(), GamePanel.tileSize , GamePanel.tileSize, null);
+        //g2.fillRect(location.getX_axis(), location.getY_axis(), GamePanel.tileSize , GamePanel.tileSize);
     }
     
 }
