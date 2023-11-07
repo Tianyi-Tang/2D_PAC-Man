ca5aabae265a7dd1e4ec4192fe99090efabc2516
Andy Tang <tta89@sfu.ca>
2023-11-07 10:17:22 -0800

    create a time line for the game thread

--- pawsandpumpkins/src/main/java/cmpt276/group4/WindowAndInput/MainPanel.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/WindowAndInput/MainPanel.java
@@ -1,17 +1,39 @@
 package cmpt276.group4.WindowAndInput;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
-public class MainPanel extends JPanel {
-    private final int original_tileSize = 16;
-    private final  int scale = 3;
-    public final int tileSize = original_tileSize * scale;
-
-    private final int maxScreenCol = 16;
-    private final int maxScreenRow = 16;
-    private final int screenWidth = maxScreenCol * tileSize;
-    private final int screenHeight = maxScreenRow * tileSize;
-
+public class MainPanel extends JPanel implements Runnable {
     
+    private final int screenWidth = 48 * 16;
+    private final int screenHeight = 48 * 16;
+
+    private Thread main_thread;
+
+
+
+    public MainPanel(){
+        this.setPreferredSize(new Dimension(screenWidth,screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+    public void createTimeLine(){
+        if(main_thread == null){
+            main_thread = new Thread(this);
+            main_thread.start();
+        }
+    }
+
+    @Override
+    public void run() {
+
+        while (main_thread != null) {
+            
+        }
+        
+    }
     
 }
