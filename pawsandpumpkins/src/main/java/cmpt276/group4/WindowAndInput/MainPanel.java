185f6f67a4e54a0e7461eb237c9ec980c05f6130
Andy Tang <tta89@sfu.ca>
2023-11-07 19:27:36 -0800

    remove the mouse listener in KeyboardListener

--- pawsandpumpkins/src/main/java/cmpt276/group4/WindowAndInput/MainPanel.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/WindowAndInput/MainPanel.java
@@ -2,7 +2,10 @@ package cmpt276.group4.WindowAndInput;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class MainPanel extends JPanel implements Runnable {
@@ -10,27 +13,37 @@ public class MainPanel extends JPanel implements Runnable {
     private final int screenWidth = 48 * 16;
     private final int screenHeight = 48 * 16;
 
-    private Thread main_thread;
+    private Thread mainThread;
+    private JButton startButton;
 
 
 
     public MainPanel(){
+        startButton = new JButton();
+        startButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Has been click");
+                super.mouseClicked(e);
+            }
+        });
+
         this.setPreferredSize(new Dimension(screenWidth,screenHeight));
         this.setBackground(Color.black);
         this.setDoubleBuffered(true);
     }
 
     public void createTimeLine(){
-        if(main_thread == null){
-            main_thread = new Thread(this);
-            main_thread.start();
+        if(mainThread == null){
+            mainThread = new Thread(this);
+            mainThread.start();
         }
     }
 
     @Override
     public void run() {
 
-        while (main_thread != null) {
+        while (mainThread != null) {
             
         }
         
