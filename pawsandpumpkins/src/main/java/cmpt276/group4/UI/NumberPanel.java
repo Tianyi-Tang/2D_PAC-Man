4f7ca701f0923646567931fb1c11d305f94abd83
LI-YU WU <li-yu_wu@sfu.ca>
2023-11-11 12:59:16 -0800

    refine numberpanel class

--- pawsandpumpkins/src/main/java/cmpt276/group4/UI/NumberPanel.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/UI/NumberPanel.java
@@ -8,34 +8,59 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+import cmpt276.group4.GameStatus;
+
 public class NumberPanel extends JPanel {
     private static final int NUM_IMAGES = 10;
     private BufferedImage[] digitImages = new BufferedImage[NUM_IMAGES];
-    private BufferedImage backgroundImage; // Background image
+    private BufferedImage backgroundImage;
+    private BufferedImage greyOut; // Background image
     private int[][] numbersToDisplay; // Array of number lines
-    private int imageWidth, imageHeight;
+    private int imageWidth;
     private int[][] positions; // Positions for each line of numbers
     private String directory;
+    private int[] yAxisLineUp;
+    private String backgroundImgName;
+    private int xAxisLineUp;
+    private int digitXSize, digitYSize;
+    private double popUpScale;
+
+    public enum Status {
+        Win, GameOver
+    }
 
     public NumberPanel() {
         directory = System.getProperty("user.dir");
-        //loadNumberImages();
-        //loadBackgroundImage("path/to/your/background/image.png"); // Adjust path
-        this.imageWidth = 8; // default width
-        this.imageHeight = 8; // default height
-        this.numbersToDisplay = new int[5][]; // Four lines of numbers
-        this.positions = new int[5][2]; // Positions for each line
+        loadNumberImages();
+
+        this.imageWidth = 8;
+        this.numbersToDisplay = new int[5][]; 
+        this.positions = new int[5][2]; 
+        digitXSize = 11;
+        digitYSize = 11;
+        xAxisLineUp = 420;
+        yAxisLineUp = new int[] { 335, 351, 365, 397, 427 };
+        popUpScale = 0.75;
+
+    }
+
+    // Need to change to gaatestatus
+    public void init(boolean isWin) {
+
+        if (isWin) {
+            backgroundImgName = "win.png";
+        } else {
+            backgroundImgName = "game_over.png";
+        }
+
+        loadBackgroundImage(backgroundImgName);
+
     }
 
     private void loadNumberImages() {
-        
+
         for (int i = 0; i < NUM_IMAGES; i++) {
             try {
-                System.out.println(directory + "\\pawsandpumpkins\\res\\n" + //
-                        "um" + i + ".png");
-
-
-                // digitImages[i] = ImageIO.read(new File(directory+"/res/num/" + i + ".png"));
                 digitImages[i] = ImageIO.read(new File(directory + "/pawsandpumpkins/res/num/" + i + ".png"));
             } catch (IOException e) {
                 e.printStackTrace(); // Or handle the exception as needed
@@ -43,49 +68,39 @@ public class NumberPanel extends JPanel {
         }
     }
 
-    private void loadBackgroundImage(String path) {
+    private void loadBackgroundImage(String filename) {
         try {
-            //backgroundImage = ImageIO.read(new File(directory + "/pawsandpumpkins/res/pop_up/" + "game_welcome_page2.png"));
-            backgroundImage = ImageIO.read(new File(directory + "/pawsandpumpkins/res/pop_up/" + "win.jpg"));
+            backgroundImage = ImageIO.read(new File(directory + "/pawsandpumpkins/res/pop_up/" + filename));
+            greyOut = ImageIO.read(new File(directory + "/pawsandpumpkins/res/pop_up/" + "grey.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void setNumbers(int lineNumber, int[] numbers, int startX, int startY, int imageWidth, int imageHeight) {
-        if (lineNumber >= 0 && lineNumber < numbersToDisplay.length) {
-            numbersToDisplay[lineNumber] = numbers;
-            positions[lineNumber][0] = startX;
-            positions[lineNumber][1] = startY;
-            this.imageHeight = imageHeight;
-            this.imageWidth = imageWidth;
-        }
-        repaint();
-    }
-    public void setNumbers(int lineNumber, int numbers, int startX, int startY, int imageWidth, int imageHeight) {
-        if (lineNumber >= 0 && lineNumber < numbersToDisplay.length) {
-            numbersToDisplay[lineNumber] = intToArray(numbers);
-            positions[lineNumber][0] = startX;
-            positions[lineNumber][1] = startY;
-            this.imageHeight = imageHeight;
-            this.imageWidth = imageWidth;
-        }
-        repaint();
-    }
 
-    
+    public void setNumbers(int totalRewards, int regular, int bonus, int punishments, int overall) {
+        int[] nums = { totalRewards, regular, bonus, punishments, overall };
+        for (int i = 0; i < nums.length; i++) {
+            numbersToDisplay[i] = intToArray(nums[i]);
+        }
+        repaint();
+
+    }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // Draw background image
+
         if (backgroundImage != null) {
-            g.drawImage(backgroundImage, 0, 0, this.getWidth(), this.getHeight(), this);
+            g.drawImage(greyOut, 0, 0, this.getWidth(), this.getHeight(), this);
+            g.drawImage(backgroundImage, 85, 90, (int) (this.getWidth() * popUpScale),
+                    (int) (this.getHeight() * popUpScale), this);
+
         }
         // Draw each line of numbers
         for (int i = 0; i < numbersToDisplay.length; i++) {
             if (numbersToDisplay[i] != null) {
-                drawNumberLine(g, numbersToDisplay[i], positions[i][0], positions[i][1]);
+                drawNumberLine(g, numbersToDisplay[i], xAxisLineUp, yAxisLineUp[i]);
             }
         }
     }
@@ -96,25 +111,25 @@ public class NumberPanel extends JPanel {
             if (number >= 0 && number < digitImages.length) {
                 BufferedImage img = digitImages[number];
                 if (img != null) {
-                    g.drawImage(img, x, startY, imageWidth, imageHeight, this);
-                    x += imageWidth; // Move to the right for the next digit
+                    g.drawImage(img, x, startY, digitXSize, digitYSize, this);
+                    x += (imageWidth + 1); // Move to the right for the next digit
+
                 }
             }
         }
+
     }
+
     private int[] intToArray(int number) {
-        // Convert the number to a String to easily access individual digits
         String numberStr = Integer.toString(number);
-        
-        // Create an array of the same length as the number of digits
+
         int[] digits = new int[numberStr.length()];
-        
-        // Fill the array with the individual digits
+
         for (int i = 0; i < numberStr.length(); i++) {
-            // Subtract '0' to convert char to int (ASCII manipulation)
             digits[i] = numberStr.charAt(i) - '0';
         }
-        
+
         return digits;
     }
+
 }
