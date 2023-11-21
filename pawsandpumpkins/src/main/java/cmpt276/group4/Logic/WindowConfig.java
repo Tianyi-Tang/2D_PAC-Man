b2b70a83508781d3f244204a6dc474e4299047ba
Andy Tang <tta89@sfu.ca>
2023-11-21 09:20:39 -0800

    add the titleSize in WindowConfig

--- pawsandpumpkins/src/main/java/cmpt276/group4/Logic/WindowConfig.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/Logic/WindowConfig.java
@@ -1,5 +1,9 @@
 package cmpt276.group4.Logic;
 
 public class WindowConfig {
+    private static final int original_tileSize = 16;
+    private static final int scale = 3;
+    public static final int tileSize = original_tileSize * scale;
+
     
 }
