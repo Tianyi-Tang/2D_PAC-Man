2ff5e9510a6aa22a07f59244d24bb9fda9c8ea96
Andy Tang <tta89@sfu.ca>
2023-11-20 16:21:41 -0800

    add test class for checking player inital position in PlayerTests

--- pawsandpumpkins/src/test/java/cmpt276/group4/Player/PlayerTests.java
+++ pawsandpumpkins/src/test/java/cmpt276/group4/Player/PlayerTests.java
@@ -1,14 +1,28 @@
 package cmpt276.group4.Player;
 
+import static org.junit.Assert.assertEquals;
+
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import cmpt276.group4.Position;
+import cmpt276.group4.WindowAndInput.GamePanel;
 
 /**
  * Test class for player
  */
 public class PlayerTests {
-    Player player;
+    public Player player;
+    public int number;
+
     @BeforeEach
-    void intialPlayer(){
+    public void setUp(){
         player = Player.getInstance();
+    }
+
+    @Test
+    public void initalPosition(){
+        assertEquals(new Position(GamePanel.tileSize, GamePanel.tileSize), player.getPosition());
     }
 }
