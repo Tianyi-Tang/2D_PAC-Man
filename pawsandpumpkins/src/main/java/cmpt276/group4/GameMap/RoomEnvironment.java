03e660451e43b4e9c7cf515c1147b232a3c428b4
Andy Tang <tta89@sfu.ca>
2023-11-22 12:40:15 -0800

    add the rewards and enemies in RoomEnvironment class

--- pawsandpumpkins/src/main/java/cmpt276/group4/GameMap/RoomEnvironment.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/GameMap/RoomEnvironment.java
@@ -1,5 +1,15 @@
 package cmpt276.group4.GameMap;
 
+
+import java.util.ArrayList;
+
+import cmpt276.group4.Enemy.Enemy;
+import cmpt276.group4.Reward.Reward;
+
 public class RoomEnvironment {
+    private ArrayList<Reward> rewards;
+    private ArrayList<Enemy> enemies;
+
     
+
 }
