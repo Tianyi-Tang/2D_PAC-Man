55ece390211e1423cb1faa51aa0bf6e58cfe6df5
Andy Tang <tta89@sfu.ca>
2023-11-22 10:07:23 -0800

    add variables that count number of tile, wall and obstacle in RoomLayout

--- pawsandpumpkins/src/main/java/cmpt276/group4/GameMap/RoomLayout.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/GameMap/RoomLayout.java
@@ -5,12 +5,17 @@ import java.util.ArrayList;
 import cmpt276.group4.CharacterAvaliablePosition;
 import cmpt276.group4.Position;
 import cmpt276.group4.Logic.WindowConfig;
+import cmpt276.group4.Room.RoomItemType;
 
 public class RoomLayout {
 
     private ArrayList<CharacterAvaliablePosition> elements;
     private ArrayList<Position> unAviablePositions;
     private static RoomLayout  instance;
+
+    private int tileNum;
+    private int WallNum;
+    private int ObstacleNum;
 
     public RoomLayout(){
         elements = new ArrayList<CharacterAvaliablePosition>();
@@ -23,7 +28,7 @@ public class RoomLayout {
         return instance;
     }
 
-    public void addElementInMap(CharacterAvaliablePosition element){
+    public void addElementInMap(CharacterAvaliablePosition element,RoomItemType type){
         elements.add(element);
         isPositionUnAviable(element);
     }
