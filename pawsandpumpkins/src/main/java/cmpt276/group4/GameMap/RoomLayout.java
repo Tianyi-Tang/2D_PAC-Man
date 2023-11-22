15c7765d301e237be919a3a74a8d63442558923b
Andy Tang <tta89@sfu.ca>
2023-11-21 20:58:34 -0800

    add function in RoomLayout to add element in to array

--- pawsandpumpkins/src/main/java/cmpt276/group4/GameMap/RoomLayout.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/GameMap/RoomLayout.java
@@ -1,5 +1,26 @@
 package cmpt276.group4.GameMap;
 
+import java.util.ArrayList;
+
+import cmpt276.group4.CharacterAvaliablePosition;
+import cmpt276.group4.Position;
+
 public class RoomLayout {
+
+    ArrayList<CharacterAvaliablePosition> elements;
+    ArrayList<Position> unAviablePositions;
+
+    public void addElementInMap(CharacterAvaliablePosition element){
+        elements.add(element);
+        isPositionUnAviable(element);
+    }
+
+    private void isPositionUnAviable(CharacterAvaliablePosition element){
+        if(!element.getPlayerAvaliable()){
+            unAviablePositions.add(element.getPosition());
+        }
+    }
     
+
+
 }
