5a0f74eb19a4b4effba179ec13015c46c846b02a
Andy Tang <tta89@sfu.ca>
2023-11-21 22:07:17 -0800

    add functions to RoomLayour to check whether the position is aviable for player or enemy

--- pawsandpumpkins/src/main/java/cmpt276/group4/GameMap/RoomLayout.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/GameMap/RoomLayout.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import cmpt276.group4.CharacterAvaliablePosition;
 import cmpt276.group4.Position;
+import cmpt276.group4.Logic.WindowConfig;
 
 public class RoomLayout {
 
@@ -21,6 +22,28 @@ public class RoomLayout {
         }
     }
     
+    public ArrayList<CharacterAvaliablePosition> getElements(){
+        return elements;
+    }
+
+    public boolean isPositionAviable(Position position){
+        if(!outOfScreen(position))
+            return false;
+        for (Position unAviablePos  : unAviablePositions) {
+            if(position == unAviablePos)
+                return false;
+        }
+        return true;
+    }
+
+    private boolean outOfScreen(Position position){
+        if(position.getX_axis() < 0 || position.getY_axis() < 0)
+            return false;
+        else if(position.getX_axis() > WindowConfig.screenWidth - WindowConfig.tileSize || position.getY_axis() > WindowConfig.screenHeight - WindowConfig.tileSize)
+            return false;
+        else
+            return true;
+    }
 
 
 }
