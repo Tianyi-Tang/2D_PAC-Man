a3ee014b9a7f28e846b9b61a4e25c7e51a107ed7
LI-YU WU <li-yu_wu@sfu.ca>
2023-11-25 15:23:22 -0800

    Enemy Movvement Integration test

--- pawsandpumpkins/src/test/java/cmpt276/group4/Enemy/EnemyMovementIntegrationTest.java
+++ pawsandpumpkins/src/test/java/cmpt276/group4/Enemy/EnemyMovementIntegrationTest.java
@@ -7,92 +7,62 @@ import cmpt276.group4.Position;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
 
-// Assuming this test is placed in the same package as the classes it tests
 public class EnemyMovementIntegrationTest {
 
     private Ghost ghost;
-    private RecordUsedPlace recordUsedPlace;
     private EnemyMovement enemyMovement;
-    private RoomEnvironment roomEnvironment;
 
     @Mock
     private RecordUsedPlace mockRecord;
     @Mock
     private RoomEnvironment mockRoomEnvironment;
     @Mock
-    private EnemyMovement mockEnemyMovement;
-    // private Ghost ghost;
-    private Position startPosition = new Position(0, 0);
-    private Position playerPosition = new Position(10, 10);
     private GameManager mockGameManager;
+
+    private Position startPosition = new Position(0, 0);
+    private Position playerNearPosition = new Position(10, 10);
+    private Position playerFarPosition = new Position(500, 500);
 
     @BeforeEach
     void setUp() {
-        // Mock dependencies
+        MockitoAnnotations.openMocks(this);
 
-         MockitoAnnotations.openMocks(this);
         RecordUsedPlace.setInstance(mockRecord);
         RoomEnvironment.setInstance(mockRoomEnvironment);
-        mockGameManager = mock(GameManager.class);
         GameManager.setInstance(mockGameManager);
-        when(mockRecord.getRandomFromAvailablePosition()).thenReturn(new Position(0, 0));
-        when(mockRoomEnvironment.addEnemy(any(Ghost.class))).thenReturn(true);
-        when(mockRoomEnvironment.getPlayerPosition()).thenReturn(playerPosition);
-        when(mockRecord.isPlayerNearBy(anyInt(), any(Position.class))).thenReturn(true);
-        //when(mockPlayerPosition.equal(any(Position.class))).thenReturn(true);
 
-
-        recordUsedPlace = Mockito.mock(RecordUsedPlace.class);
-        roomEnvironment = Mockito.mock(RoomEnvironment.class);
-
-        // Use real methods for the key class being tested
-        ghost = new Ghost(EnemyType.GHOST_BASIC);
         enemyMovement = new EnemyMovement();
+        
+        when(mockRecord.getRandomFromAvailablePosition()).thenReturn(new Position(48, 48));
+        when(mockRoomEnvironment.addEnemy(any(Ghost.class))).thenReturn(true);
+        when(mockRoomEnvironment.getPlayerPosition()).thenReturn(playerNearPosition);
+        when(mockRecord.isPlayerNearBy(anyInt(), any(Position.class))).thenReturn(true);
 
-        // Inject mocks
+        ghost = new Ghost(EnemyType.GHOST_BASIC);
         ghost.setPlayerMovement(enemyMovement);
-        RecordUsedPlace.setInstance(recordUsedPlace);
+        ghost.setEnemyPosition(startPosition);
     }
 
     @Test
     void testEnemyMovementTowardsPlayer() {
-        // Arrange
-        Position playerPosition = new Position(10, 10);
-        Position initialGhostPosition = new Position(0, 0);
-        ghost.setEnemyPosition(initialGhostPosition);
+        when(mockRecord.isPlayerNearBy(anyInt(), any(Position.class))).thenReturn(true);
 
-        // Assume player is nearby
-        when(recordUsedPlace.isPlayerNearBy(anyInt(), any(Position.class))).thenReturn(true);
-        when(roomEnvironment.getPlayerPosition()).thenReturn(playerPosition);
-
-        // Act
         ghost.action();
-
-        // Assert - Check that the ghost has moved closer to the player
-        // Depending on the implementation, you might need to verify certain behaviors or final positions
+        verify(mockRecord).isPlayerNearBy(anyInt(), any(Position.class));
     }
 
-    // @Test
-    // void testEnemyRandomMovementWhenPlayerIsNotNearby() {
-    //     // Arrange
-    //     Position initialGhostPosition = new Position(0, 0);
-    //     ghost.setEnemyPosition(initialGhostPosition);
+    @Test
+    void testEnemyRandomMovementWhenPlayerIsNotNearby() {
+        when(mockRoomEnvironment.getPlayerPosition()).thenReturn(playerFarPosition);
+        when(mockRecord.isPlayerNearBy(anyInt(), any(Position.class))).thenReturn(false);
 
-    //     // Assume player is not nearby
-    //     when(recordUsedPlace.isPlayerNearBy(anyInt(), any(Position.class))).thenReturn(false);
-
-    //     // Act
-    //     ghost.action();
-
-    //     // Assert - Check that the ghost has moved to a random position
-    // }
-
-    // Add more tests to cover different scenarios and branches in the movement logic
+        ghost.action();
+        verify(mockRecord).isPlayerNearBy(anyInt(), any(Position.class));
+    }
 }
