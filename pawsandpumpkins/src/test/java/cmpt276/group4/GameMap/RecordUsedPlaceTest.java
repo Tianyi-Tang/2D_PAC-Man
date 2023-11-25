ba29941a25c9a80ad4d547504f50d9ee9905bc10
Andy Tang <tta89@sfu.ca>
2023-11-24 20:05:32 -0800

    add commit to functions in RecordUsedPlaceTest

--- pawsandpumpkins/src/test/java/cmpt276/group4/GameMap/RecordUsedPlaceTest.java
+++ pawsandpumpkins/src/test/java/cmpt276/group4/GameMap/RecordUsedPlaceTest.java
@@ -1,23 +1,31 @@
 package cmpt276.group4.GameMap;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import cmpt276.group4.Position;
-import cmpt276.group4.Logic.GameConfig;
 import cmpt276.group4.Logic.WindowConfig;
 
 public class RecordUsedPlaceTest {
     public RecordUsedPlace record;
 
+    /**
+     * set up the RecordUsedPlace before each test cause run
+     */
     @BeforeEach
     public void setUp(){
         record = new RecordUsedPlace();
     }
 
+    /**
+     * Test the position can add the to aviable position and other class can use
+     * getRandomFromAvailablePosition function to get aviable position
+     */
     @Test
     public void addAviableToRecord(){
         Position availblePos = new Position(12, 35);
@@ -25,6 +33,27 @@ public class RecordUsedPlaceTest {
         assertEquals(availblePos, record.getRandomFromAvailablePosition());
     }
 
+    /**
+     * Test the add multiple position to aviable and get from record
+     */
+    @Test void addThreeAviable(){
+        ArrayList<Position> positions = new ArrayList<Position>();
+        Position position;
+        for(int i=0;i < 3;i++){
+            position = new Position(i * WindowConfig.tileSize, WindowConfig.tileSize);
+            record.addAviable(position);
+            positions.add(position);
+        }
+
+        ArrayList<Position> acutalPositions = record.getAviablePosition();
+        for(int i=0; i< 3;i++){
+            assertEquals(positions.get(i), acutalPositions.get(i));
+        }
+    }
+
+    /**
+     * Test the record will reject the same position adding to aviable 
+     */
     @Test
     public void rejectSameAviableToReocrd(){
         Position position1 = new Position(WindowConfig.tileSize, WindowConfig.tileSize);
@@ -34,15 +63,20 @@ public class RecordUsedPlaceTest {
         assertEquals(1, record.getLengthOfAviable());
     }
 
+    /**
+     * Test the after adding position to aviable, record can remove it from
+     * aviable 
+     */
     @Test
     public void removeFromAviable(){
         Position position = new Position(WindowConfig.tileSize * 2, WindowConfig.tileSize);
         record.addAviable(position);
-        assertEquals(1, record.getAviablePosition().size());
         record.removeFromAviable(position);
         assertEquals(null, record.getRandomFromAvailablePosition());
     }
-
+    /**
+     * Test record can check the position is near to player
+     */
     @Test
     public void playerNear(){
         Position nearByPos = new Position(WindowConfig.tileSize * 2, WindowConfig.tileSize);
