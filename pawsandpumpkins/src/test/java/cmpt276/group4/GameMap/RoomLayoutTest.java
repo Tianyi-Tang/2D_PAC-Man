8e61cbbe3408a00013b44ac603e1302f220ee54c
Andy Tang <tta89@sfu.ca>
2023-11-25 23:25:21 -0800

    modify the RoomLayoutTest to allow adding ToneStone for testing and add several testing

--- pawsandpumpkins/src/test/java/cmpt276/group4/GameMap/RoomLayoutTest.java
+++ pawsandpumpkins/src/test/java/cmpt276/group4/GameMap/RoomLayoutTest.java
@@ -2,6 +2,8 @@ package cmpt276.group4.GameMap;
 
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 
@@ -11,68 +13,134 @@ import org.junit.jupiter.api.Test;
 import cmpt276.group4.CharacterAvaliablePosition;
 import cmpt276.group4.Position;
 import cmpt276.group4.Logic.WindowConfig;
+import cmpt276.group4.Room.Obstacle;
 import cmpt276.group4.Room.Tile;
+import cmpt276.group4.Room.Tombstone;
 import cmpt276.group4.Room.Wall;
 
 public class RoomLayoutTest {
     RoomLayout roomLayout;
-    RecordUsedPlace record;
+    RecordUsedPlace mockrecord;
+
+    public Position availablePos;
+
+    public enum RoomItemType{
+        Wall,
+        Tombstone,
+        Tile
+    }
 
     @BeforeEach
     public void setUP(){
         roomLayout = new RoomLayout();
-        record = new RecordUsedPlace();
-        roomLayout.init(record);
+        mockrecord = mock(RecordUsedPlace.class);
+
+        availablePos = new Position(WindowConfig.tileSize, WindowConfig.tileSize);
+        when(mockrecord.isPlaceAviable(availablePos)).thenReturn(true);
+
+        roomLayout.init(mockrecord);
+        RecordUsedPlace.getInstance().addAviable(availablePos);
+        RoomEnvironment.getInstance().init(mockrecord);
     }
 
     /**
      * 
      */
     @Test
-    public void addWall(){
-        Position wallPosition = new Position(WindowConfig.tileSize*2, WindowConfig.tileSize);
-        record.addAviable(wallPosition);
-        roomLayout.addElementInMap(new Wall(wallPosition));
-        assertEquals(1, roomLayout.getWallNumber());
+    public void successAddWall(){
+        CharacterAvaliablePosition wall = addRoomItem(RoomItemType.Wall,availablePos);
+        assertEquals(wall, roomLayout.getElements().get(0));
     }
 
     @Test
-    public void addTiles(){
-        ArrayList<CharacterAvaliablePosition> allItem = new ArrayList<CharacterAvaliablePosition>();
-
-        Position tilePosition;
-        Tile tile;
-        for(int i =0;i < 3;i++){
-            tilePosition = new Position(i * WindowConfig.tileSize, 1 * WindowConfig.tileSize);
-            record.addAviable(tilePosition);
-            tile = new Tile(tilePosition);
-            roomLayout.addElementInMap(tile);
-            allItem.add(tile);
-        }
-
-        for(int i = 0;i < allItem.size();i++){
-            assertEquals(allItem.get(i), roomLayout.getElements().get(i));
-        }
+    public void failAddWall(){
+        when(mockrecord.isPlaceAviable(availablePos)).thenReturn(false);
+        addRoomItem(RoomItemType.Wall,availablePos);
+        assertEquals(0, roomLayout.getWallNumber());
     }
 
     @Test
-    public void moveToWall(){
-        Position wallPosition = new Position(WindowConfig.tileSize * 2, 0);
-        record.addAviable(wallPosition);
-        roomLayout.addElementInMap( new Wall(wallPosition));
-        assertEquals(false, roomLayout.isPositionAviable(wallPosition));
+    public void successAddTile(){
+        CharacterAvaliablePosition tile = addRoomItem(RoomItemType.Tile,availablePos);
+        assertEquals(tile, roomLayout.getElements().get(0));
     }
 
     @Test
-    public void moveOutOfScreenMin(){
+    public void failAddTile(){
+        when(mockrecord.isPlaceAviable(availablePos)).thenReturn(false);
+        addRoomItem(RoomItemType.Tile,availablePos);
+        assertEquals(0, roomLayout.getTileNumber());
+    }
+
+    @Test
+    public void successAddObstacle(){
+        setCanPlaceEnemyAndObstacle_result(true,availablePos);
+        CharacterAvaliablePosition tombstone = addRoomItem(RoomItemType.Tombstone,availablePos);
+        assertEquals(tombstone, roomLayout.getElements().get(0));
+    }
+
+    @Test
+    public void failAddObstacle(){
+        setCanPlaceEnemyAndObstacle_result(false,availablePos);
+        addRoomItem(RoomItemType.Tombstone,availablePos);
+        assertEquals(0, roomLayout.getObstaclesNumber());
+    }
+
+    @Test
+    public void moveToAviablePos(){
+        addRoomItem(RoomItemType.Tile, availablePos);
+        assertEquals(true, roomLayout.isPositionAviable(availablePos));
+    }
+
+    @Test
+    public void moveToUnavailablePosition(){
+        Position anotherPosition = new Position(WindowConfig.tileSize *3, 0);
+        setCanPlaceEnemyAndObstacle_result(false, anotherPosition);
+        addRoomItem(RoomItemType.Tombstone, anotherPosition);
+        addRoomItem(RoomItemType.Wall,availablePos);
+        
+        assertEquals(false, roomLayout.isPositionAviable(availablePos)); 
+    }
+
+    @Test
+    public void moveOutOfScreen_negativeX(){
         Position outofScreenPos = new Position(- WindowConfig.tileSize, 0);
-        assertEquals(false, record.isPlaceAviable(outofScreenPos));
+        assertEquals(false, roomLayout.isPositionAviable(outofScreenPos));
     }
 
     @Test
-    public void moveOutOfScreenMax(){
+    public void moveOutOfScreen_negativeY(){
+        Position outofScreenPos = new Position(WindowConfig.tileSize, - WindowConfig.tileSize);
+        assertEquals(false, roomLayout.isPositionAviable(outofScreenPos));
+    }
+
+    @Test
+    public void moveOutOfScreen_postiveY(){
         Position outofScreenPos = new Position( WindowConfig.tileSize * 3, WindowConfig.tileSize * (WindowConfig.maxScreenRow+1));
-        assertEquals(false, record.isPlaceAviable(outofScreenPos));
+        assertEquals(false, roomLayout.isPositionAviable(outofScreenPos));
+    }
+
+    @Test
+    public void moveOutOfScreen_postiveX(){
+        Position outofScreenPos = new Position( WindowConfig.tileSize * (WindowConfig.maxScreenRow+1), WindowConfig.tileSize );
+        assertEquals(false, roomLayout.isPositionAviable(outofScreenPos));
+    }
+
+
+    private CharacterAvaliablePosition addRoomItem(RoomItemType type, Position position){
+        CharacterAvaliablePosition roomItem;
+        if(type == RoomItemType.Wall)
+            roomItem= new Wall(position);
+        else if(type == RoomItemType.Tombstone)
+            roomItem = new Tombstone(position);
+        else
+            roomItem = new Tile(position);
+        roomLayout.addElementInMap(roomItem);
+        return roomItem;
+    }
+
+    private void setCanPlaceEnemyAndObstacle_result(boolean result,Position position){
+        when(mockrecord.canPlaceEnemyAndObstacle(position)).thenReturn(result);
     }
 
 }
