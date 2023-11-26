16291b1bf13e7f6a56d3f7710386657a8677d2c9
xiaochengfeng <cxa37@sfu.ca>
2023-11-26 03:33:51 -0800

    add junit test for gametime

---
+++ pawsandpumpkins/src/test/java/cmpt276/group4/Time/GameTimeTest.java
@@ -0,0 +1,36 @@
+package cmpt276.group4.Time;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTimeTest {
+
+    @Test
+    void testSingleton() {
+        GameTime instance1 = GameTime.getInstance();
+        GameTime instance2 = GameTime.getInstance();
+        assertSame(instance1, instance2, "Instances are not the same");
+    }
+
+    @Test
+    void testTimeInterval() {
+
+    }
+
+    @Test
+    void testCountTime() {
+        GameTime gameTime = GameTime.getInstance();
+        for (int i = 0; i < 60; i++) {
+            gameTime.countTime();
+        }
+        assertEquals( 1, gameTime.getTime());
+    }
+
+    @Test
+    void testGetTime() {
+        GameTime gameTime = GameTime.getInstance();
+        int expectedTime = gameTime.getTime();
+        assertEquals(expectedTime, gameTime.getTime());
+    }
+}
