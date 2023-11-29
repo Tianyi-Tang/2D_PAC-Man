8edfed8c458b464c62764c21b330e41e291cf483
Andy Tang <tta89@sfu.ca>
2023-11-29 01:23:37 -0800

    add comment too InitialiseGameItem

--- pawsandpumpkins/src/main/java/cmpt276/group4/WindowAndInput/InitialiseGameItem.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/WindowAndInput/InitialiseGameItem.java
@@ -13,24 +13,43 @@ import cmpt276.group4.Reward.RewardInitialization;
 import cmpt276.group4.Room.RoomFactory;
 import cmpt276.group4.Room.RoomInitialization;
 
+/**
+ * Class to create and initlalise all object in the game 
+ */
 public class InitialiseGameItem {
     private GameConfig config;
     private RoomInitialization roomInitialization;
     private RoomFactory roomFactory;
 
+    /**
+     * Setting the game config to InitialiseGameItem will use
+     * @param config game config that InitialiseGameItem will use
+     */
     public InitialiseGameItem(GameConfig config){
         this.config = config;
     }
 
+    /**
+     * Give the game config in InitialiseGameItem
+     * @return
+     */
     public GameConfig getConfig(){
         return config;
     }
 
+    /**
+     * Setting the room initialization and room factory will be used
+     * @param roomInitialization room initialization will used by InitialiseGameItem
+     * @param roomFactory room factory will be used by InitialiseGameItem
+     */
     public void setRoomInitialize(RoomInitialization roomInitialization, RoomFactory roomFactory){
         this.roomInitialization = roomInitialization;
         this.roomFactory = roomFactory;        
     }
 
+    /**
+     * Create the room for the game
+     */
     public void createRoom(){
         if(config != null && roomInitialization != null){
             roomInitialization.initializeRoom(config.getGameLevel(), roomFactory);
@@ -38,16 +57,27 @@ public class InitialiseGameItem {
         }
     }
 
+    /**
+     * Create the tile in game
+     */
     public void createTile(){
         if(roomInitialization != null)
             roomInitialization.iTiles(roomFactory);
     }
 
+    /**
+     * Create the wall in game
+     */
     public void createWall(){
         if(roomInitialization != null)
             roomInitialization.iWalls(roomFactory);
     }
 
+    /**
+     * Create player character and pass it to room rnvironment
+     * @param roomLayout roomLayout that player will use
+     * @param roomEnvironment room environment that going to get player
+     */
     public void createPlayer(RoomLayout roomLayout ,RoomEnvironment roomEnvironment){
         Player player = PlayerGenerator.creatPlayer(GameManager.getInstance(), roomEnvironment,roomLayout);
         roomEnvironment.setPlayer(player);
@@ -64,6 +94,7 @@ public class InitialiseGameItem {
 
      /**
      * loading all enemies in the game
+     * @param factory factory that going to create enemy
      */
     public void createEnemy(EnemyFactory factory){
         new EnemyInitialization(factory);
@@ -71,6 +102,7 @@ public class InitialiseGameItem {
 
      /**
      * loading all rewards in the game
+     * @param factory factory that going to create reward
      */
     public void createReward(RewardFactory factory){
         RewardInitialization reward_initialization = new RewardInitialization(factory);
