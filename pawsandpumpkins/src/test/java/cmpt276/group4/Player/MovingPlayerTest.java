63365ed526196ce99a9374419e842e9316b9a00c
Andy Tang <tta89@sfu.ca>
2023-11-28 21:42:53 -0800

    add comment to MovingPlayerTest

--- pawsandpumpkins/src/test/java/cmpt276/group4/Player/MovingPlayerTest.java
+++ pawsandpumpkins/src/test/java/cmpt276/group4/Player/MovingPlayerTest.java
@@ -19,7 +19,7 @@ import cmpt276.group4.WindowAndInput.keyboardListener;
 import cmpt276.group4.WindowAndInput.KeyInput.MovingKey;
 
 /**
- * Integration test
+ * Combine KeyboardLisenter, Player and PlayerMovement for integration test
  */
 public class MovingPlayerTest {
     public keyboardListener lister;
@@ -31,6 +31,9 @@ public class MovingPlayerTest {
     public RoomEnvironment mockEnvironment;
     public RoomLayout mockLayout;
 
+    /**
+     * Seting up the object will be use later and init the player
+     */
     @BeforeEach
     public void setUp(){
         keyInput = new KeyInput();
@@ -46,6 +49,10 @@ public class MovingPlayerTest {
         player.init(mockManager, mockEnvironment, mockLayout);
     }
 
+    /**
+     * Test player will change position fellow by keypress, if target position is
+     * available 
+     */
     @Test
     public void pessKeyMovePlayer(){
         Position[] positions = createAvailablePositions(1,-WindowConfig.tileSize,0);
@@ -54,6 +61,10 @@ public class MovingPlayerTest {
         assertEquals(positions[0], player.getPosition());
     }
 
+    /**
+     * Test player will not change position fellow by keypress, if target position 
+     * is not available 
+     */
     @Test
     public void pressKeyMovePlayerFail(){
         Position playInitPosition = player.getPosition();
@@ -64,6 +75,10 @@ public class MovingPlayerTest {
         assertEquals(playInitPosition, player.getPosition());
     }
 
+    /**
+     * Test player will keep move if the user not release the key and these position
+     * are avaliable 
+     */
     @Test
     public void keypressing(){
         Position[] positions = createAvailablePositions(2, 0, WindowConfig.tileSize);
@@ -72,6 +87,9 @@ public class MovingPlayerTest {
         assertEquals(positions[positions.length -1], player.getPosition());
     }
 
+    /**
+     * Test player will not going to change the position, after the key is release 
+     */
     @Test
     public void keypressOne(){
         Position[] positions = createAvailablePositions(4, WindowConfig.tileSize, 0);
@@ -81,6 +99,10 @@ public class MovingPlayerTest {
         assertEquals(positions[0], player.getPosition());
     }
 
+    /**
+     * Test player will collect reward if the player move to the same position
+     * with reward
+     */
     @Test
     public void playerCollectReward(){
         createAvailablePositions(4, 0, -WindowConfig.tileSize);
@@ -92,11 +114,24 @@ public class MovingPlayerTest {
     }
 
 
+    /**
+     * After press or release key, call player for update
+     * @param key which key will be press 
+     * @param press is key press or release 
+     * @param time number step you want to player to move 
+     */
     private void movePlayer(MovingKey key,boolean press,int time){
         keyInput.inputKey(key, press);
         playerUpdate.playerUpdate(time, true);
     }
 
+    /**
+     * create a series of availlable positions return them
+     * @param number number of positions you want to create
+     * @param x_changing the x_change for these position
+     * @param y_change the y_change for these position
+     * @return the array of position 
+     */
     private Position[] createAvailablePositions(int number,int x_changing, int y_change){
         int x_init = player.getPosition().getX_axis();
         int y_init = player.getPosition().getY_axis();
@@ -109,6 +144,10 @@ public class MovingPlayerTest {
         return positions;
     }
 
+    /**
+     * Set a array of position to available for player 
+     * @param positions set array of position
+     */
     private void setAvailable(Position[] positions){
         for(int i=0;i < positions.length;i++)
             when(mockLayout.isPositionAviable(positions[i])).thenReturn(true);
