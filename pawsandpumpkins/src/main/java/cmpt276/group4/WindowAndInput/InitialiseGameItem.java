28dcaf2c82d075248e8849f726eae592d8062ee4
LI-YU WU <li-yu_wu@sfu.ca>
2023-11-29 23:13:48 -0800

    Revert "undo revert"
    This reverts commit e1c268263522c40e6d0ba163e11bdd79004ddc34.

--- pawsandpumpkins/src/main/java/cmpt276/group4/WindowAndInput/InitialiseGameItem.java
+++ pawsandpumpkins/src/main/java/cmpt276/group4/WindowAndInput/InitialiseGameItem.java
@@ -14,103 +14,68 @@ import cmpt276.group4.Reward.RewardInitialization;
 import cmpt276.group4.Room.RoomFactory;
 import cmpt276.group4.Room.RoomInitialization;
 
-/**
- * Class to create and initlalise all object in the game
- */
 public class InitialiseGameItem {
     private GameConfig config;
     private RoomInitialization roomInitialization;
     private RoomFactory roomFactory;
 
-    /**
-     * Setting the game config to InitialiseGameItem will use
-     * 
-     * @param config game config that InitialiseGameItem will use
-     */
-    public InitialiseGameItem(GameConfig config) {
+    public InitialiseGameItem(GameConfig config){
         this.config = config;
     }
 
-    /**
-     * Give the game config in InitialiseGameItem
-     * 
-     * @return
-     */
-    public GameConfig getConfig() {
+    public GameConfig getConfig(){
         return config;
     }
 
-    /**
-     * Setting the room initialization and room factory will be used
-     * 
-     * @param roomInitialization room initialization will used by InitialiseGameItem
-     * @param roomFactory        room factory will be used by InitialiseGameItem
-     */
-    public void setRoomInitialize(RoomInitialization roomInitialization, RoomFactory roomFactory) {
+    public void setRoomInitialize(RoomInitialization roomInitialization, RoomFactory roomFactory){
         this.roomInitialization = roomInitialization;
-        this.roomFactory = roomFactory;
+        this.roomFactory = roomFactory;        
     }
 
-    /**
-     * Create the room for the game
-     */
-    public void createRoom() {
-        if (config != null && roomInitialization != null) {
+    public void createRoom(){
+        if(config != null && roomFactory != null){
             roomInitialization.initializeRoom(config.getGameLevel(), roomFactory);
             roomInitialization.iRoom(roomFactory);
         }
     }
 
-    /**
-     * Create the tile in game
-     */
-    public void createTile() {
-        if (roomInitialization != null)
+    public void createTile(){
+        if(roomFactory != null)
             roomInitialization.iTiles(roomFactory);
     }
 
-    /**
-     * Create the wall in game
-     */
-    public void createWall() {
-        if (roomInitialization != null)
+    public void createWall(){
+        if(roomFactory != null)
             roomInitialization.iWalls(roomFactory);
     }
 
-    /**
-     * Create player character and pass it to room rnvironment
-     * 
-     * @param roomLayout      roomLayout that player will use
-     * @param roomEnvironment room environment that going to get player
-     */
-    public void createPlayer(RoomLayout roomLayout, RoomEnvironment roomEnvironment) {
-        Player player = PlayerGenerator.creatPlayer(GameManager.getInstance(), roomEnvironment, roomLayout);
+    public void createPlayer(RoomLayout roomLayout ,RoomEnvironment roomEnvironment){
+        Player player = PlayerGenerator.creatPlayer(GameManager.getInstance(), roomEnvironment,roomLayout);
         roomEnvironment.setPlayer(player);
     }
 
-    /**
+
+     /**
      * loading all obstacles in the game
      */
-    public void createObstacle() {
-        if (roomInitialization != null)
+    public void  createObstacle(){
+        if(roomInitialization != null)
             roomInitialization.iTombs(roomFactory);
     }
 
-    /**
+     /**
      * loading all enemies in the game
-     * * @param factory factory that going to create enemy
      */
-    public void createEnemy(EnemyFactory factory) {
-        new EnemyInitialization(factory);
+    public void createEnemy(){
+        new EnemyInitialization(new EnemyFactory());
     }
 
-    /**
+     /**
      * loading all rewards in the game
-     * * @param factory factory that going to create reward
      */
-    public void createReward(RewardFactory factory) {
-        RewardInitialization reward_initialization = new RewardInitialization(factory);
+    public void createReward(){
+        RewardInitialization reward_initialization = new RewardInitialization(new RewardFactory());
         reward_initialization.generateReward();
     }
-
+    
 }
