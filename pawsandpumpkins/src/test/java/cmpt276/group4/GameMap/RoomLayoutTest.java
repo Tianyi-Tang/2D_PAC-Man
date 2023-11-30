91492b4acf104c2cdcc946c2d458ca410f376893
LI-YU WU <li-yu_wu@sfu.ca>
2023-11-29 23:48:47 -0800

    ad comment and undo reverrt

--- pawsandpumpkins/src/test/java/cmpt276/group4/GameMap/RoomLayoutTest.java
+++ pawsandpumpkins/src/test/java/cmpt276/group4/GameMap/RoomLayoutTest.java
@@ -1,6 +1,5 @@
 package cmpt276.group4.GameMap;
 
-
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -16,19 +15,25 @@ import cmpt276.group4.Room.Tile;
 import cmpt276.group4.Room.Tombstone;
 import cmpt276.group4.Room.Wall;
 
+/**
+ * Class for room layout unit test
+ */
 public class RoomLayoutTest {
     RoomLayout roomLayout;
     RecordUsedPlace mockrecord;
 
     public Position availablePos;
 
-    public enum RoomItemType{
+    public enum RoomItemType {
         Wall,
         Tile
     }
 
+    /**
+     * set up the roomlayout for testing
+     */
     @BeforeEach
-    public void setUP(){
+    public void setUP() {
         roomLayout = new RoomLayout();
         mockrecord = mock(RecordUsedPlace.class);
 
@@ -40,14 +45,17 @@ public class RoomLayoutTest {
     }
 
     /**
-     * 
+     * Test room layout can add to the wall if the position is available
      */
     @Test
-    public void successAddWall(){
-        CharacterAvaliablePosition wall = addRoomItem(RoomItemType.Wall,availablePos);
+    public void successAddWall() {
+        CharacterAvaliablePosition wall = addRoomItem(RoomItemType.Wall, availablePos);
         assertEquals(wall, roomLayout.getElements().get(0));
     }
 
+ /**
+     * Test room layout will reject adding wall if the position is not available
+     */
     @Test
     public void failAddWall(){
         when(mockrecord.isPlaceAviable(availablePos)).thenReturn(false);
@@ -55,12 +63,18 @@ public class RoomLayoutTest {
         assertEquals(0, roomLayout.getWallNumber());
     }
 
+    /**
+     * Test room layout can add to the tile if the position is available
+     */
     @Test
-    public void successAddTile(){
-        CharacterAvaliablePosition tile = addRoomItem(RoomItemType.Tile,availablePos);
+    public void successAddTile() {
+        CharacterAvaliablePosition tile = addRoomItem(RoomItemType.Tile, availablePos);
         assertEquals(tile, roomLayout.getElements().get(0));
     }
 
+  /**
+     * Test room layout will reject adding tile if position is not available
+     */
     @Test
     public void failAddTile(){
         when(mockrecord.isPlaceAviable(availablePos)).thenReturn(false);
@@ -68,78 +82,135 @@ public class RoomLayoutTest {
         assertEquals(0, roomLayout.getTileNumber());
     }
 
+    /**
+     * Test room layout will add obstacle if position is avaible and not block
+     * some key point that player will eneter
+     */
     @Test
-    public void successAddObstacle(){
-        setCanPlaceEnemyAndObstacle_result(true,availablePos);
+    public void successAddObstacle() {
+        setCanPlaceEnemyAndObstacle_result(true, availablePos);
 
         addTomestone(availablePos);
         assertEquals(1, roomLayout.getObstaclesNumber());
     }
 
+    /**
+     * Test room layout reject adding obstacle if position is available but block
+     * player to go to some area
+     */
     @Test
-    public void failAddObstacle(){
-        setCanPlaceEnemyAndObstacle_result(false,availablePos);
+    public void failAddObstacle() {
+        setCanPlaceEnemyAndObstacle_result(false, availablePos);
         assertEquals(0, roomLayout.getObstaclesNumber());
     }
 
     @Test
-    public void moveToAviablePos(){
+    public void AddObstacleUnavaliable(){
+        when(mockrecord.isPlaceAviable(availablePos)).thenReturn(false);
+        addTomestone(availablePos);
+        assertEquals(0, roomLayout.getObstaclesNumber());
+    }
+
+    /**
+     * Test room layout will allow character move to that position if
+     * the it is not in unaviable list
+     */
+    @Test
+    public void moveToAviablePos() {
         addRoomItem(RoomItemType.Tile, availablePos);
         assertEquals(true, roomLayout.isPositionAviable(availablePos));
     }
 
+    /**
+     * Test room layout will reject character move to that position if
+     * the position is not in unaviable list
+     */
     @Test
-    public void moveToUnavailablePosition(){
-        Position anotherPosition = new Position(WindowConfig.tileSize *3, 0);
-        setCanPlaceEnemyAndObstacle_result(false, anotherPosition);
-        addTomestone(anotherPosition);
-        addRoomItem(RoomItemType.Wall,availablePos);
-        
-        assertEquals(false, roomLayout.isPositionAviable(availablePos)); 
+    public void moveToUnavailablePosition() {
+        Position anotherPosition = new Position(WindowConfig.tileSize * 3, 0);
+        when(mockrecord.isPlaceAviable(anotherPosition)).thenReturn(true);
+        addRoomItem(RoomItemType.Wall, anotherPosition);
+        addRoomItem(RoomItemType.Wall, availablePos);
+
+        assertEquals(false, roomLayout.isPositionAviable(availablePos));
     }
 
+    /**
+     * Test room layout will reject character move to that position if
+     * position x-axis have negatvie value
+     */
     @Test
-    public void moveOutOfScreen_negativeX(){
-        Position outofScreenPos = new Position(- WindowConfig.tileSize, 0);
+    public void moveOutOfScreen_negativeX() {
+        Position outofScreenPos = new Position(-WindowConfig.tileSize, 0);
         assertEquals(false, roomLayout.isPositionAviable(outofScreenPos));
     }
 
+    /**
+     * Test room layout will reject character move to that position if
+     * position y-axis have negatvie value
+     */
     @Test
-    public void moveOutOfScreen_negativeY(){
-        Position outofScreenPos = new Position(WindowConfig.tileSize, - WindowConfig.tileSize);
+    public void moveOutOfScreen_negativeY() {
+        Position outofScreenPos = new Position(WindowConfig.tileSize, -WindowConfig.tileSize);
         assertEquals(false, roomLayout.isPositionAviable(outofScreenPos));
     }
 
+    /**
+     * Test room layout will reject character move to that position if
+     * position x-axis are bigger than screen width value
+     */
     @Test
-    public void moveOutOfScreen_postiveY(){
-        Position outofScreenPos = new Position( WindowConfig.tileSize * 3, WindowConfig.tileSize * (WindowConfig.maxScreenRow+1));
+    public void moveOutOfScreen_postiveY() {
+        Position outofScreenPos = new Position(WindowConfig.tileSize * 3,
+                WindowConfig.tileSize * (WindowConfig.maxScreenRow + 1));
         assertEquals(false, roomLayout.isPositionAviable(outofScreenPos));
     }
 
+    /**
+     * Test room layout will reject character move to that position if
+     * position y-axis are bigger than screen height value
+     */
     @Test
-    public void moveOutOfScreen_postiveX(){
-        Position outofScreenPos = new Position( WindowConfig.tileSize * (WindowConfig.maxScreenRow+1), WindowConfig.tileSize );
+    public void moveOutOfScreen_postiveX() {
+        Position outofScreenPos = new Position(WindowConfig.tileSize * (WindowConfig.maxScreenRow + 1),
+                WindowConfig.tileSize);
         assertEquals(false, roomLayout.isPositionAviable(outofScreenPos));
     }
 
-    private void addTomestone(Position position){
+    /**
+     * Create the mock tomestone and add the position to the roomlayout
+     * 
+     * @param position the position of tomestone
+     */
+    private void addTomestone(Position position) {
         Tombstone mockTombstone = mock(Tombstone.class);
         when(mockTombstone.getPosition()).thenReturn(position);
         roomLayout.addElementInMap(mockTombstone);
     }
 
-
-    private CharacterAvaliablePosition addRoomItem(RoomItemType type, Position position){
+    /**
+     * Create a wall or tile to add into roomlayout
+     * 
+     * @param type     want to add wall or tile to room
+     * @param position the position of that object
+     * @return the object that create
+     */
+    private CharacterAvaliablePosition addRoomItem(RoomItemType type, Position position) {
         CharacterAvaliablePosition roomItem;
-        if(type == RoomItemType.Wall)
-            roomItem= new Wall(position);
+        if (type == RoomItemType.Wall)
+            roomItem = new Wall(position);
         else
             roomItem = new Tile(position);
-            
+
         roomLayout.addElementInMap(roomItem);
         return roomItem;
     }
 
+ /**
+     * Set the mock RecordUsedPlace to allow or reject the position can place the obstacle
+     * @param result allow or reject place obstacle in that position
+     * @param position the ppsition you want to set
+     */
     private void setCanPlaceEnemyAndObstacle_result(boolean result,Position position){
         when(mockrecord.canPlaceEnemyAndObstacle(position)).thenReturn(result);
     }
--- pawsandpumpkins/src/test/java/cmpt276/group4/Player/MovingPlayerTest.java
+++ pawsandpumpkins/src/test/java/cmpt276/group4/Player/MovingPlayerTest.java
@@ -19,7 +19,7 @@ import cmpt276.group4.WindowAndInput.keyboardListener;
 import cmpt276.group4.WindowAndInput.KeyInput.MovingKey;
 
 /**
- * Integration test
+ * Combine KeyboardLisenter, Player and PlayerMovement for integration test
  */
 public class MovingPlayerTest {
     public keyboardListener lister;
@@ -31,8 +31,11 @@ public class MovingPlayerTest {
     public RoomEnvironment mockEnvironment;
     public RoomLayout mockLayout;
 
+    /**
+     * Seting up the object will be use later and init the player
+     */
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
         keyInput = new KeyInput();
         player = new Player();
         lister = new keyboardListener();
@@ -46,16 +49,24 @@ public class MovingPlayerTest {
         player.init(mockManager, mockEnvironment, mockLayout);
     }
 
+    /**
+     * Test player will change position fellow by keypress, if target position is
+     * available
+     */
     @Test
-    public void pessKeyMovePlayer(){
-        Position[] positions = createAvailablePositions(1,-WindowConfig.tileSize,0);
+    public void pessKeyMovePlayer() {
+        Position[] positions = createAvailablePositions(1, -WindowConfig.tileSize, 0);
 
         movePlayer(MovingKey.A, true, 1);
         assertEquals(positions[0], player.getPosition());
     }
 
+    /**
+     * Test player will not change position fellow by keypress, if target position
+     * is not available
+     */
     @Test
-    public void pressKeyMovePlayerFail(){
+    public void pressKeyMovePlayerFail() {
         Position playInitPosition = player.getPosition();
         Position unaviablpos = new Position(WindowConfig.tileSize, WindowConfig.tileSize * 2);
         when(mockLayout.isPositionAviable(unaviablpos)).thenReturn(false);
@@ -64,16 +75,23 @@ public class MovingPlayerTest {
         assertEquals(playInitPosition, player.getPosition());
     }
 
+    /**
+     * Test player will keep move if the user not release the key and these position
+     * are avaliable
+     */
     @Test
-    public void keypressing(){
+    public void keypressing() {
         Position[] positions = createAvailablePositions(2, 0, WindowConfig.tileSize);
 
         movePlayer(MovingKey.S, true, 3);
-        assertEquals(positions[positions.length -1], player.getPosition());
+        assertEquals(positions[positions.length - 1], player.getPosition());
     }
 
+    /**
+     * Test player will not going to change the position, after the key is release
+     */
     @Test
-    public void keypressOne(){
+    public void keypressOne() {
         Position[] positions = createAvailablePositions(4, WindowConfig.tileSize, 0);
 
         movePlayer(MovingKey.D, true, 1);
@@ -81,39 +99,60 @@ public class MovingPlayerTest {
         assertEquals(positions[0], player.getPosition());
     }
 
+    /**
+     * Test player will collect reward if the player move to the same position
+     * with reward
+     */
+
     @Test
-    public void playerCollectReward(){
+    public void playerCollectReward() {
         createAvailablePositions(4, 0, -WindowConfig.tileSize);
-        
+
         Reward reward = mock(Reward.class);
         when(mockEnvironment.collectReward()).thenReturn(reward);
         movePlayer(MovingKey.W, true, 1);
         verify(reward).addBenefit(player);
     }
 
-
-    private void movePlayer(MovingKey key,boolean press,int time){
+    /**
+     * After press or release key, call player for update
+     * 
+     * @param key   which key will be press
+     * @param press is key press or release
+     * @param time  number step you want to player to move
+     */
+    private void movePlayer(MovingKey key, boolean press, int time) {
         keyInput.inputKey(key, press);
         playerUpdate.playerUpdate(time, true);
     }
 
-    private Position[] createAvailablePositions(int number,int x_changing, int y_change){
+    /**
+     * create a series of availlable positions return them
+     * 
+     * @param number     number of positions you want to create
+     * @param x_changing the x_change for these position
+     * @param y_change   the y_change for these position
+     * @return the array of position
+     */
+    private Position[] createAvailablePositions(int number, int x_changing, int y_change) {
         int x_init = player.getPosition().getX_axis();
         int y_init = player.getPosition().getY_axis();
         Position[] positions = new Position[number];
 
-        for(int i=1;i < number+1;i++)
-            positions[i-1] = new Position(x_init + x_changing*i, y_init + y_change *i);
+        for (int i = 1; i < number + 1; i++)
+            positions[i - 1] = new Position(x_init + x_changing * i, y_init + y_change * i);
 
         setAvailable(positions);
         return positions;
     }
 
+  /**
+     * Set a array of position to available for player 
+     * @param positions set array of position
+     */
     private void setAvailable(Position[] positions){
         for(int i=0;i < positions.length;i++)
             when(mockLayout.isPositionAviable(positions[i])).thenReturn(true);
     }
-
-    
 
 }
